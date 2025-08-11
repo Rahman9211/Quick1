e03b1c24fd0ae2f13c63b5af2858258fd5a484a1
abdullah <abdullah384858@gmail.com>
2025-08-12 00:54:55 +0530

    Delete routes directory

--- routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-import expess from "express";
-import {auth} from "../middlewares/auth.js";
-import { generateArticle } from "../controllers/aiController.js";
-
-const aiRouter = expess.Router();
-
-aiRouter.post('/generate-article', auth, generateArticle)
-
-export default aiRouter
