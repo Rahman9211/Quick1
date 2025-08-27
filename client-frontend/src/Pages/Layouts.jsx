cb7ce789f79bc1b3003b4313e5134f2ce2d78463
Rahman <abdullah384858@gmail.com>
2025-08-27 16:53:07 +0530

    adding some animation

--- client-frontend/src/Pages/Layouts.jsx
+++ client-frontend/src/Pages/Layouts.jsx
@@ -4,38 +4,38 @@ import { assets } from '../assets/assets';
 import { Menu, X } from 'lucide-react';
 import Sidebar from '../Component/Sidebar';
 import { SignIn, useUser } from '@clerk/clerk-react';
-const Layouts = () => {
-
+const Layout = () => {
   const navigate = useNavigate();
   const [sidebar, setSidebar] = useState(false);
-  const { user } = useUser()
-
+  const { user } = useUser();
   return user ? (
-    <div className='flex flex-col items-start justify-start h-screen'>
-
-      <nav className='w-full px-8 min-h-14 flex items-center
-      justify-between border-b border-gray-200'>
-        <img src={assets.logo} alt="logo" onClick={() => navigate('/')} className='cursor-pointer' />
-        {
-          sidebar
-            ? <X onClick={() => setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden' />
-            : <Menu onClick={() => setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden' />
-        }
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-[56px] flex items-center justify-between border-b border-gray-200">
+        <img src={assets.logo} className="cusor-pointer w-32 sm:w-44" alt="" onClick={() => navigate("/")} />
+        {sidebar ? (
+          <X
+            onClick={() => setSidebar(false)}
+            className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer"
+          />
+        ) : (
+          <Menu
+            onClick={() => setSidebar(true)}
+            className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer"
+          />
+        )}
       </nav>
-
-      <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
-        <Sidebar sidebar={sidebar} setSidebar={setSidebar} /> {/* ✅ Fixed line */}
-        <div className='flex-1'>
+      <div className="flex w-full flex-1 h-[calc(100vh-56px)]">
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className="flex-1 bg-[#F4F7FB]">
           <Outlet />
         </div>
       </div>
-
     </div>
   ) : (
-    <div className='flex item-center justify-center h-screen'>
-      <SignIn />
+    <div className="flex items-center justify-center h-screen">
+      <SignIn></SignIn>
     </div>
-  )
+  );
 };
 
-export default Layouts;
+export default Layout;
